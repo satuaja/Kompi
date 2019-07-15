6c917aefa6a8e9bba4ec1d6070561e5f9e6eb1db
BungNawi <40924186+satuaja@users.noreply.github.com>
2019-07-15 17:49:18 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -19,7 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*2)+ 1);
 var xxx=null;
 if(x=="1"){xxx="daftar-aplikasi-tv-online-streaming.html"}
 if(x=="2"){xxx="kredit-tanpa-agunan.html"}a_to_ve[i].href="https://satuaja-safelink.blogspot.com/2019/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
