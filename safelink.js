04d2801c2335325c0b3f14ea887ded7503d46207
BungNawi <40924186+satuaja@users.noreply.github.com>
2019-07-15 17:32:22 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -21,10 +21,8 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="current-trends-in-international.html"}
-if(x=="2"){xxx="feeling-insured.html"}
-if(x=="3"){xxx="more-tips-to-improve-your-websites.html"}
-if(x=="4"){xxx="american-health-care-trends-old-fat-and.html"}a_to_ve[i].href="https://satuaja-safelink.blogspot.com/2019/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="daftar-aplikasi-tv-online-streaming.html"}
+if(x=="2"){xxx="kredit-tanpa-agunan.html"}a_to_ve[i].href="https://satuaja-safelink.blogspot.com/2019/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
